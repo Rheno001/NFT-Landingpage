8751d180ea640ab434cf4558ae5c5bd3266c7aaf
Rheno <88987852+rennyrae@users.noreply.github.com>
2022-11-15 21:36:07 +0100

    update,read more card

---
+++ test.js
@@ -0,0 +1,8 @@
+document.querySelector('.read').addEventListener('click', function() {
+    document.querySelector('#content').style.height= 'auto';
+    this.style.display= 'none';
+  });
+  document.querySelector('.read_2').addEventListener('click', function() {
+    document.querySelector('#content_2').style.height= 'auto';
+    this.style.display= 'none';
+  });
